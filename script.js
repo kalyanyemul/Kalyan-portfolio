74e1c0e176031791fc7b51370d7b62a2c129cbda
kalyanyemul <kalyanyemul007@gmail.com>
2025-09-15 12:14:05 +0530

    certificate-section

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function openPopup(src) {
+  const popup = document.getElementById("popup");
+  const popupImg = document.getElementById("popup-img");
+  popup.style.display = "flex";
+  popupImg.src = src;
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
+function openPopup(imgSrc) {
+  document.getElementById("popup").style.display = "flex";
+  document.getElementById("popup-img").src = imgSrc;
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    closePopup();
+  }
+});
